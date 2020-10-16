9a12dfd60700a12d6e94d66d3ab994ec835d0132
Luther <chikumbaluther@gmail.com>
2020-10-16 12:48:53 -0600

    Updated Version.

--- Quill/db/schema.rb
+++ Quill/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_174230) do
+ActiveRecord::Schema.define(version: 2020_10_16_153336) do
 
   create_table "abouts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -44,6 +44,11 @@ ActiveRecord::Schema.define(version: 2020_10_15_174230) do
   create_table "homes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "topic"
+    t.string "article"
+    t.string "about"
+    t.string "archive"
+    t.string "setting"
   end
 
   create_table "log_ins", force: :cascade do |t|
